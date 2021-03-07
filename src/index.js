a11ffb93309b3e35d7293dea6dd268d6a35f3cd9
elveret <lena.veret@gmail.com>
2021-03-07 19:34:41 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result;
+    if (array === undefined || array.length == 0) {
+        result = 0;
+    } else {
+        result = array.reduce((res, current) => res < current ? res : current);
+    }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result;
+    if (array === undefined || array.length == 0) {
+        result = 0;
+    } else {
+        result = array.reduce((res, current) => res > current ? res : current);
+    }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result;
+    if (array === undefined || array.length == 0) {
+        result = 0;
+    } else {
+        result = (array.reduce((res, current) => res + current))/array.length;
+    }
+  return result;
 }
